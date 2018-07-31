71b12637af86263d6f9b8b69c57d2918808bf295
PhanHongAnh <honganh.p256@gmail.com>
2018-07-31 14:53:03 +0700

    Review 1

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,60 @@
+class ReviewsController < ApplicationController
+  before_action :logged_in_user, except: [:index, :show]
+  before_action :find_review, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
+  def index
+    @reviews = Review.order_review.page(params[:page]).per Settings.paginate.per
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = current_user.reviews.build review_params
+    if @review.save
+      flash[:success] = t "create_review_success"
+      redirect_to @review
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @review.update_attributes review_params
+      flash[:success] = t "update_review_success"
+      redirect_to @review
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review.destroy
+    flash[:success] = t "delete_review_success"
+    redirect_to request.referrer || reviews_url
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit :title, :content
+  end
+
+  def correct_user
+    @review = current_user.reviews.find_by id: params[:id]
+    redirect_to reviews_url if @review.nil?
+  end
+
+  def find_review
+    @review = Review.find_by id: params[:id]
+    return if @review
+    flash.now[:danger] = t "cant_find_review"
+    redirect_to request.referrer || reviews_url
+  end
+end
