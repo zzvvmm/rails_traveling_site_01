f06346483e9f3e0c6623fefecee89bc9f1db455f
manjusakajin <sakurano96@gmail.com>
2018-08-02 09:22:54 +0700

    show_trip 2

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,14 @@ Rails.application.routes.draw do
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     resources :users, only: [:new, :create]
-    resources :trips, only: [:new, :create, :show]
+    resources :trips, only: [:new, :create, :show] do
+      resources :participations, only: [:create, :destroy]
+      resources :searchs, only: :index
+    end
     get "/about", to: "static_pages#about"
     resources :chatrooms, param: :slug
     resources :messages
     resources :reviews
-    resources :search, only: :index
-    resources :participations, only: [:create, :destroy]
     mount ActionCable.server => "/cable"
     mount Ckeditor::Engine => "/ckeditor"
   end
